dd1f3c4baecaaa714dd1e2ea48d2881a413dc3f4
Muhammad Nauval Almas <130370028+valalmas-15@users.noreply.github.com>
2024-01-01 14:53:33 +0700

    Delete New folder directory

--- New folder/app/src/main/java/com/example/masakyuk/viewmodel/ProfileViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.masakyuk.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class ProfileViewModel : ViewModel() {
-    var nama: String = ""
-    var email: String = ""
-    // tambahkan properti lain sesuai kebutuhan
-}
